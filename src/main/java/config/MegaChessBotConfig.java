5e0691e2ed235a1d6c0af35d30ae7c7b2128b958
SergioSpeziale <rs_sergioh@hotmail.es>
2020-12-10 19:16:30 -0300

    Indentation and oreganization fixes

--- src/main/java/config/MegaChessBotConfig.java
+++ src/main/java/config/MegaChessBotConfig.java
@@ -2,12 +2,9 @@ package config;
 
 import java.net.URI;
 
-public class MegaChessBotConfig {
-	
-	static String authToken = "9549bb0c-b8dc-43b2-849e-59d7ea2a2665";
-	
-	static String url = "ws://megachess.herokuapp.com/service?authtoken=";
-	
+public class MegaChessBotConfig {	
+	static String authToken = "9549bb0c-b8dc-43b2-849e-59d7ea2a2665";	
+	static String url = "ws://megachess.herokuapp.com/service?authtoken=";	
 	static URI uri = URI.create("ws://megachess.herokuapp.com/service?authtoken=" + authToken);
 
 	public static String getAuthToken() {
@@ -21,9 +18,4 @@ public class MegaChessBotConfig {
 	public static URI getUri() {
 		return uri;
 	}
-	
-	
-	
-	
-
 }
--- src/main/java/core/MegaChessBot.java
+++ src/main/java/core/MegaChessBot.java
@@ -28,28 +28,23 @@ import service.ChallengeOpponentService;
 import service.GameActionService;
 import service.UserListService;
 
-public class MegaChessBot extends TextWebSocketHandler{
-	
-	private WebSocketSession clientSession;	
-	
+public class MegaChessBot extends TextWebSocketHandler {
+
+	private WebSocketSession clientSession;
 	private ClientMessageHandler msgHandler;
-	
 	private int opponentsNumber;
 
-	
 	public MegaChessBot() {
 		super();
-		
+
 		this.opponentsNumber = 0;
-		StandardWebSocketClient webSocketClient = new StandardWebSocketClient();  
-		
+		StandardWebSocketClient webSocketClient = new StandardWebSocketClient();
+
 		this.msgHandler = new ClientMessageHandler();
 
 		try {
-			
-			this.clientSession = webSocketClient.doHandshake(this, new WebSocketHttpHeaders(), 
-					MegaChessBotConfig.getUri()).get();
-			
+			this.clientSession = webSocketClient
+					.doHandshake(this, new WebSocketHttpHeaders(), MegaChessBotConfig.getUri()).get();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} catch (ExecutionException e) {
@@ -60,94 +55,91 @@ public class MegaChessBot extends TextWebSocketHandler{
 	// Overrides
 
 	@Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) {        
-        JsonObject json = Json.createReader(new StringReader(message.getPayload())).readObject();
-        this.msgHandler.handleMessage(json, this);
-    }	
-	
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+		JsonObject json = Json.createReader(new StringReader(message.getPayload())).readObject();
+		this.msgHandler.handleMessage(json, this);
+	}
+
 	// Public
-	
-	public void sendTextMessage(String content) {    	
-        try {
-        	synchronized(this.clientSession) {
-        		this.clientSession.sendMessage(new TextMessage(content));
-    			System.out.println("Mensaje saliente: " + content);
-        	}
+
+	public void sendTextMessage(String content) {
+		try {
+			synchronized (this.clientSession) {
+				this.clientSession.sendMessage(new TextMessage(content));
+				System.out.println("Mensaje saliente: " + content);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    }
-	
-	
-	// Actions
-	
+	}
+
 	public void getUserList() {
 		UserListService userListservice = new UserListService(this);
-		userListservice.run();		
+		userListservice.run();
 	}
-	
-	public void notifyUsersListUpdate(String[] usersList) {		
-		String players = "";		
-		for(int i = 0 ; i < usersList.length ; i++) {			
-			players += usersList[i];			
-			if(i != (usersList.length - 1)) {
+
+	public void notifyUsersListUpdate(String[] usersList) {
+		String players = "";
+		for (int i = 0; i < usersList.length; i++) {
+			players += usersList[i];
+			if (i != (usersList.length - 1)) {
 				players += ", ";
 			}
-		}		
+		}
 		System.out.println("Jugadores disponibles: " + players);
-		
-		if(this.opponentsNumber == 0) {
+		if (this.opponentsNumber == 0) {
 			this.chooseOpponent(usersList);
-		}		
+		}
 	}
-	
+
 	public void notifyChallenge(GetChallengeResponse challenge) {
-		
-		System.out.println("Acepto challenge de " + challenge.getUsername());	
-		if(challenge.getUsername().equalsIgnoreCase("SergioHR")) {
-			ChallengeAcceptanceService challengeAcceptedService = new ChallengeAcceptanceService(this);	 
-			HashMap<String,String> body = new HashMap<String,String>();
+		System.out.println("Acepto challenge de " + challenge.getUsername());
+		if (challenge.getUsername().equalsIgnoreCase("SergioHR")) {
+			ChallengeAcceptanceService challengeAcceptedService = new ChallengeAcceptanceService(this);
+			HashMap<String, String> body = new HashMap<String, String>();
 			body.put("board_id", challenge.getBoardId());
 			challengeAcceptedService.setBody(body);
 			challengeAcceptedService.run();
 		}
 	}
-	
+
 	public void makeMove(GetYourTurnResponse turnResponse) {
 		Color color = turnResponse.actualTurn.equals("white") ? Piece.Color.WHITE : Piece.Color.BLACK;
 		String boardLine = turnResponse.board;
 		String turnToken = turnResponse.turnToken;
-		Board board = this.createBoard(turnResponse.boardId, color, boardLine);		
-		Turn turn = new Turn(turnToken, board, color);		
-		GameActionService gameActionService = new GameActionService(this);	 
+		Board board = this.createBoard(turnResponse.boardId, color, boardLine);
+		Turn turn = new Turn(turnToken, board, color);
+		GameActionService gameActionService = new GameActionService(this);
 		HashMap<String, Object> nextMove = turn.getNextMove();
 		gameActionService.setData(nextMove);
 		gameActionService.run();
 	}
-	
+
 	public void showEndGameResults(GetGameOverResponse gameOver) {
 		System.out.println("Resultado final");
-		System.out.println("Piezas blancas: "+ gameOver.whiteUsername + " - Puntaje: " + gameOver.whiteScore);
-		System.out.println("Piezas negras: "+ gameOver.blackUsername + " - Puntaje: " + gameOver.blackScore);
+		System.out.println("Piezas blancas: " + gameOver.whiteUsername + " - Puntaje: " + gameOver.whiteScore);
+		System.out.println("Piezas negras: " + gameOver.blackUsername + " - Puntaje: " + gameOver.blackScore);
 	}
 	
-	private void chooseOpponent(String[] usersList) {		
-		String chosenOpponent= "SergioHR";
-		ChallengeOpponentService challengeOpponentService = new ChallengeOpponentService(this);	 
-		HashMap<String,String> body = new HashMap<String,String>();
-		
-		if(chosenOpponent != "") {
+	// Internal methods
+
+	private void chooseOpponent(String[] usersList) {
+		String chosenOpponent = "SergioHR";
+		ChallengeOpponentService challengeOpponentService = new ChallengeOpponentService(this);
+		HashMap<String, String> body = new HashMap<String, String>();
+
+		if (chosenOpponent != "") {
 			body.put("message", "¿Jugamos?");
-			body.put("username", chosenOpponent);		
-			
+			body.put("username", chosenOpponent);
+
 			challengeOpponentService.setBody(body);
 			challengeOpponentService.run();
-			this.opponentsNumber ++;
-		}		
+			this.opponentsNumber++;
+		}
 	}
-	
+
 	private Board createBoard(String boardId, Color color, String boardLine) {
-		Board board = new Board(boardId);				
+		Board board = new Board(boardId);
 		board.fillSpots(boardLine, color);
 		return board;
 	}
